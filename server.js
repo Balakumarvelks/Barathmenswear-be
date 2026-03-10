78ad83850988bc2353ce6974e624d0aba687fa23
Balakumarvelks <balakumaravelks.21bsr@kongu.edu>
2026-03-10 18:35:59 +0530

    backend modified

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 
 // Enable CORS
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:3001'],
+  origin: ['http://localhost:3000', 'http://localhost:3001', 'https://barathmenswear.vercel.app'],
   credentials: true
 }));
 
